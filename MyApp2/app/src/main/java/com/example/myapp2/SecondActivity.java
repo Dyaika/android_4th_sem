6508c123f0b72a0b03445dd6c2351dda6efdfe0a
Alexandr <razchurik@gmail.com>
2023-03-07 15:14:35 +0300

    activity2 opens by clicking button, text shares via putExta but only in one direction

--- MyApp2/app/src/main/java/com/example/myapp2/SecondActivity.java
+++ MyApp2/app/src/main/java/com/example/myapp2/SecondActivity.java
@@ -3,31 +3,27 @@ package com.example.myapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
+
     private Button button;
     private EditText editText;
     final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         editText = (EditText)findViewById(R.id.input);
+        editText.setText(getIntent().getExtras().get("text").toString());
         button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, SecondActivity.class);
-                intent.putExtra("text", editText.getText());
-                startActivity(intent);
-            }
-        });
     }
-
+    public void onButtonClick(View view){
+        Toast toast = Toast.makeText(context, editText.getText().toString().replace('\n', ' '), Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
