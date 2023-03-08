7a77da56222767a8b8a12bb758f958579f25f10e
Alexandr <razchurik@gmail.com>
2023-03-08 23:17:38 +0300

    Second task completed

--- MyApp2/app/src/main/java/com/example/myapp2/MainActivity.java
+++ MyApp2/app/src/main/java/com/example/myapp2/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.myapp2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private EditText editText;
     final Context context = this;
+
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent intent = result.getData();
+                    String accessMessage = intent.getStringExtra("text_back");
+                    editText.setText(accessMessage);
+                }
+            });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(context, SecondActivity.class);
                 intent.putExtra("text", editText.getText());
-                startActivity(intent);
+
+                mStartForResult.launch(intent);
             }
         });
     }
--- MyApp2/app/src/main/java/com/example/myapp2/SecondActivity.java
+++ MyApp2/app/src/main/java/com/example/myapp2/SecondActivity.java
@@ -3,14 +3,16 @@ package com.example.myapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private static final String TAG = "activity_second";
     private Button button;
     private EditText editText;
     final Context context = this;
@@ -19,11 +21,22 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         editText = (EditText)findViewById(R.id.input);
+        editText.setBackgroundColor(getResources().getColor(R.color.cirno));
+        editText.setTextColor(getResources().getColor(R.color.white));
         editText.setText(getIntent().getExtras().get("text").toString());
         button = (Button)findViewById(R.id.button);
     }
     public void onButtonClick(View view){
+        Log.d(TAG, editText.getText().toString().replace('\n', ' '));
         Toast toast = Toast.makeText(context, editText.getText().toString().replace('\n', ' '), Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent data = new Intent();
+        data.putExtra("text_back", editText.getText().toString());
+        setResult(RESULT_OK, data);
+        finish();
     }
 }
