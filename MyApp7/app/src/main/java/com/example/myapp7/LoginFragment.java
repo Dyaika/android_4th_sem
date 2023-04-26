e90a0e2fefbb3e7ad0df4c003546e50887f9b2e2
Alexandr <razchurik@gmail.com>
2023-04-27 01:12:29 +0300

    pract7 completed?

--- MyApp7/app/src/main/java/com/example/myapp7/LoginFragment.java
+++ MyApp7/app/src/main/java/com/example/myapp7/LoginFragment.java
@@ -1,58 +1,49 @@
 package com.example.myapp7;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.security.Key;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private View close_button;
+    private Button login_button;
+    private View signup_text_button;
+    private NavController navController;
+    private TextInputEditText login_text;
+    private TextInputEditText password_text;
+    private AccountViewModel accountViewModel;
     public LoginFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        accountViewModel = ((MainActivity)requireActivity())
+                .getViewModelProvider()
+                .get(AccountViewModel.class);
     }
 
     @Override
@@ -61,4 +52,36 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        close_button = view.findViewById(R.id.close_img_button);
+        login_button = view.findViewById(R.id.login_button);
+        signup_text_button = view.findViewById(R.id.signup_text_button);
+        login_text = view.findViewById(R.id.login_text);
+        password_text = view.findViewById(R.id.password_text);
+        buttonsConfig();
+    }
+    public void buttonsConfig(){
+        close_button.setOnClickListener(view -> {
+            navController.navigate(R.id.action_loginFragment_to_profileFragment);
+        });
+        login_button.setOnClickListener(view -> {
+            String password = password_text.getText().toString();
+            String login = login_text.getText().toString();
+            accountViewModel.checkAuthentication(login, password);
+            if (Boolean.TRUE.equals(accountViewModel.isLoggedIn().getValue())){
+                navController.navigate(R.id.action_loginFragment_to_profileFragment);
+            }
+        });
+        signup_text_button.setOnClickListener(view -> {
+            navController.navigate(R.id.action_loginFragment_to_signupFragment);
+        });
+    }
+
+
+
+
 }
