118dff9edf34d462d52b985256552e162edc5f48
Alexandr <razchurik@gmail.com>
2023-05-22 10:36:16 +0300

    pract8 completed

--- MyApppp8/app/src/main/java/com/example/myapppp8/MainFragment.java
+++ MyApppp8/app/src/main/java/com/example/myapppp8/MainFragment.java
@@ -2,12 +2,18 @@ package com.example.myapppp8;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.Manifest;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,6 +39,7 @@ public class MainFragment extends Fragment {
     private Button DE;
     private Button DB;
     private SharedPreferences sharedPreferences;
+    private int EXTERNAL_STORAGE_PERMISSION_CODE = 23;
 
     public MainFragment() {
         // Required empty public constructor
@@ -107,21 +114,22 @@ public class MainFragment extends Fragment {
         }
 
 
-        /*if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             // Разрешение уже предоставлено, выполните необходимые действия для чтения/записи во внешнее хранилище
-            File externalStorage = Environment.getExternalStorageDirectory();
+            File externalStorage = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
             file = new File(externalStorage, "file.txt");
             try {
                 FileOutputStream fout = new FileOutputStream(file);
-                fout.write(text.getBytes());
+                fout.write((text + "_DE").getBytes());
                 fout.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
             // Запрос разрешения у пользователя
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        }*/
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 23);
+        }
 
 
         TextDataBase db = TextDataBase.getDatabase(getContext().getApplicationContext());
@@ -131,7 +139,7 @@ public class MainFragment extends Fragment {
             if (textEntity == null) {
                 textDao.saveText(new TextEntity("text", text + "_DB"));
             } else {
-                TextEntity newTextEntity = new TextEntity("key", text+ "_DB");
+                TextEntity newTextEntity = new TextEntity("text", text+ "_DB");
                 newTextEntity.setId(textEntity.getId());
                 textDao.updateText(newTextEntity);
             }
@@ -157,14 +165,14 @@ public class MainFragment extends Fragment {
     }
     private String getFromDevice(){
         String text = "empty";
-        /*
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             // Разрешение уже предоставлено, выполните необходимые действия для чтения/записи во внешнее хранилище
         } else {
             // Запрос разрешения у пользователя
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 23);
         }
-        File externalStorage = Environment.getExternalStorageDirectory();
+        File externalStorage = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
         File file = new File(externalStorage, "file.txt");
         try {
             FileInputStream fin = new FileInputStream(file);
@@ -174,7 +182,7 @@ public class MainFragment extends Fragment {
             text = new String(data, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
         return text;
     }
     private void getFromDatabase(){
